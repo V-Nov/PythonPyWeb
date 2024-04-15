c29f7d973d64cbd38001c90c6974d0df6dea25cb
Vladimir Novakovskiy <v.novakovskii63@gmail.com>
2024-04-15 20:19:47 +0300

    PyWeb(lab2)

--- apps/db_train/models.py
+++ apps/db_train/models.py
@@ -3,6 +3,7 @@ from django.core.validators import RegexValidator
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import datetime
 
+
 class Author(models.Model):
     phone_regex = RegexValidator(
         regex=r'^\+79\d{9}$',
@@ -51,7 +52,8 @@ class Author(models.Model):
                                       validators=[MinValueValidator(0, "Диапазон [0.0, 5.0]"),
                                                   MaxValueValidator(5, "Диапазон [0.0, 5.0]")],
                                       verbose_name='Уровень самооценки',
-                                      help_text="Введите уровень вашей самооценки, только честно! Градация от 0 до 5, где 0 - 'я молодец', 5 - 'я умница'",
+                                      help_text="Введите уровень вашей самооценки, только честно! "
+                                                "Градация от 0 до 5, где 0 - 'я молодец', 5 - 'я умница'",
                                       null=True,
                                       blank=True,
                                       )
@@ -116,3 +118,27 @@ class Author(models.Model):
             additional_year = (today.month, today.day) < (self.date_birth.month, self.date_birth.day)
             self.age = today.year - self.date_birth.year - additional_year  # Перезаписываем значение
         super().save(*args, **kwargs)
+
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField('Author', on_delete=models.CASCADE)
+    stage = models.IntegerField(default=0,
+                                blank=True,
+                                verbose_name="Стаж",
+                                help_text="Стаж в годах")
+
+    def __str__(self):
+        return f"Автор: {self.author.username}; Стаж: {self.stage} лет"
+
+
+class Entry(models.Model):
+    text = models.TextField(verbose_name="Текст статьи",
+                            )
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name='entries')
+    tags = models.ManyToManyField("Tag", related_name='entries')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Название",
+                            )
